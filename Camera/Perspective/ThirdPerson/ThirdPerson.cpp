447254c686f504842982885fc6fa6cab1347c611
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-03-30 00:25:45 +0800

    radians fix

--- Camera/Perspective/ThirdPerson/ThirdPerson.cpp
+++ Camera/Perspective/ThirdPerson/ThirdPerson.cpp
@@ -26,32 +26,39 @@ void ThirdPerson::Draw() {
 
     glm::vec2 delta = InputManager::getInstance()->getHoverDelta();
 
-    
+
     delta.x /= SCREEN_WIDTH;
     delta.x -= 0.5;
     float lookAngle = this->angleAroundSubject * delta.x;
 
-    float horizontalDistance = distanceFromSubject * cos(pitchTilt);
-    float verticalDistance = distanceFromSubject * sin(pitchTilt);
+    //std::cout << delta.x << std::endl;
+
+    float horizontalDistance = distanceFromSubject * cos(glm::radians(pitchTilt));
+    float verticalDistance = distanceFromSubject * sin(glm::radians(pitchTilt));
 
     float theta = cameraSubject->getYRotation() + lookAngle;
-    float xOffset = horizontalDistance * cos(theta);
-    float zOffset = horizontalDistance * sin(theta);
+    float xOffset = horizontalDistance * cos(glm::radians(theta));
+    float zOffset = horizontalDistance * sin(glm::radians(theta));
 
     glm::vec3 pos;
 
     pos.x = cameraSubject->getPosition().x + xOffset;
-    pos.y = cameraSubject->getPosition().y + verticalDistance - 0.6;
+    pos.y = cameraSubject->getPosition().y + verticalDistance;
     pos.z = cameraSubject->getPosition().z - zOffset;
+
+    Utils::printVec3(cameraSubject->getPosition());
+
 
     glm::mat4 viewMatrix = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);  
     
     //pitch rotation
-  
+    
     viewMatrix = glm::rotate(viewMatrix, glm::radians(pitchTilt), glm::vec3(1, 0, 0));
     viewMatrix = glm::translate(viewMatrix, pos);
+
+    //yaw rotation
     viewMatrix = glm::rotate(viewMatrix, glm::radians(180 - theta), glm::vec3(0, 1, 0));
-    
+
 
     this->viewMatrix = viewMatrix;
 
